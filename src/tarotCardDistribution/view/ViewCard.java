23ff70b1a101899850480c7d12e1f715ba23d5b2
Alexandre Jacquot <jacquot.alexandre97@orange.fr>
2016-11-18 00:00:55 +0100

    Modified constructor of ViewCard class,
    Fixed turnCard() method and GameViewTest class

--- src/tarotCardDistribution/view/ViewCard.java
+++ src/tarotCardDistribution/view/ViewCard.java
@@ -1,8 +1,16 @@
 package tarotCardDistribution.view;
 
+import com.sun.deploy.association.utility.AppAssociationReader;
 import com.sun.istack.internal.NotNull;
+import com.sun.javafx.geom.Vec2d;
+import com.sun.javafx.geom.Vec3d;
 import javafx.geometry.Point3D;
+import javafx.scene.Group;
+import javafx.scene.transform.Rotate;
 import tarotCardDistribution.model.Card;
+import tarotCardDistribution.model.Hand;
+
+import static tarotCardDistribution.model.PlayerHandler.PlayersCardinalPoint.*;
 
 /**
  * The {@code ViewCard} class is a JavaFX extended node
@@ -13,11 +21,32 @@ import tarotCardDistribution.model.Card;
  */
 public class ViewCard extends RectangleMesh {
     private Card modelCard;
+    private AppView appView;
     private static final float CARD_HEIGHT = 250;
     private static final float CARD_WIDTH = CARD_HEIGHT * (float)(55.0/88.0);
     private static final float CARD_DEPTH = 2;
     private static final int CARD_FACE_TEXTURE_WIDTH = 1536;
     private static final int CARD_FACE_TEXTURE_HEIGHT = 2663;
+
+    /**
+     * Constructs a view card with default values
+     * @since v0.6.2
+     *
+     * @param modelCard the model card related to this view card
+     * @param appView the related appview of the card
+     * @param group the group that belongs to the card
+     */
+    public ViewCard(@NotNull Card modelCard, @NotNull AppView appView, @NotNull Group group)
+    {
+        super(CARD_WIDTH, CARD_HEIGHT, CARD_DEPTH, "file:./res/testCarte" + ".jpg", CARD_FACE_TEXTURE_WIDTH, CARD_FACE_TEXTURE_HEIGHT);
+        this.modelCard = modelCard;
+        this.appView = appView;
+        group.getChildren().add(this);
+        appView.getViewCardToGroup().put(this, group);
+        setPosition(appView.getCardDefaultPosition(this));
+        setRotationAxis(Rotate.Z_AXIS);
+        setRotate(appView.getCardDefaultRotation(this));
+    }
 
     /**
      * Constructs a view card with default values
@@ -31,40 +60,7 @@ public class ViewCard extends RectangleMesh {
         this.modelCard = modelCard;
     }
 
-    /**
-     * Constructs a view card
-     * @since v0.3.0
-     *
-     * @param width the card width
-     * @param height the card height
-     * @param depth the card depth
-     * @param textureFaceWidth the width of card face on texture
-     * @param textureFaceHeight the height of card face on texture
-     * @param modelCard the model card related to this view card
-     */
-    public ViewCard(float width, float height, float depth,
-                    float textureFaceWidth, float textureFaceHeight, @NotNull Card modelCard, float x, float y, float z)
-    {
-        super(width, height, depth, "file:./res/testCarte" + ".jpg", textureFaceWidth, textureFaceHeight);
-        this.modelCard = modelCard;
-        this.setTranslateX(x);
-        this.setTranslateY(y);
-        this.setTranslateZ(z);
-    }
-
     //GETTERS - no documentation needed
-
-    public void setPosition(Point3D point3D)
-    {
-        this.setTranslateX(point3D.getX());
-        this.setTranslateY(point3D.getY());
-        this.setTranslateZ(point3D.getZ());
-    }
-
-    public Point3D getPosition()
-    {
-        return new Point3D(getTranslateX(), getTranslateY(), getTranslateZ());
-    }
 
     public Card getModelCard()
     {
@@ -77,5 +73,9 @@ public class ViewCard extends RectangleMesh {
 
     public static float getCardHeight() {
         return CARD_HEIGHT;
+    }
+
+    public static float getCardDepth() {
+        return CARD_DEPTH;
     }
 }
